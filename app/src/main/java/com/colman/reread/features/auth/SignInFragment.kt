2efbc8c515cd44f633871cbc191cb88feabb8bfd
yarin <yarin12501@gmail.com>
2026-04-30 16:12:53 +0300

    added sign in and sign up screen

---
+++ app/src/main/java/com/colman/reread/features/auth/SignInFragment.kt
@@ -0,0 +1,41 @@
+package com.colman.reread.features.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.colman.reread.R
+import com.colman.reread.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+        }
+
+        binding.tvGoToSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
